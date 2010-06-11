8903dfd547fe8f81ff5a9e6eb8c5fb63c2771857
Sławosz Sławiński <slawosz@slawosz-laptop.(none)>
2010-06-12 00:00:25 +0200

    counting business time (in seconds) between two dates almost works

--- lib/extensions/time.rb
+++ lib/extensions/time.rb
@@ -43,6 +43,10 @@ class Time
       time > end_of_workday(time)
     end
 
+    def during_business_hours?(time)
+      workday?(time) && !before_business_hours?(time) && !after_business_hours?(time)
+    end
+
     # Rolls forward to the next beginning_of_workday
     # when the time is outside of business hours
     def roll_forward(time)
@@ -63,4 +67,43 @@ class Time
     end
 
   end
+
+  def business_time_left_to(time)
+    if time.to_date == self.to_date
+    end
+    time_left = 0
+    start_day = self
+    while start_day.to_date < time.to_date
+      parsed_start_day = Time.parse(start_day.to_s)
+      time_left += start_day.business_time_left_to_end
+      start_day = Time.beginning_of_workday(parsed_start_day) + 1.day
+    end
+    time_left += time.business_time_passed_from_beginning
+    time_left
+  end
+
+  def business_time_left_to_end
+    time = Time.parse(self.to_s)
+    workday = Time.workday?(time)
+    if workday && Time.during_business_hours?(time)
+      Time.end_of_workday(time) - self
+    elsif Time.before_business_hours?(time) && workday
+      Time.end_of_workday(time) - Time.beginning_of_workday(time)
+    else
+      0
+    end
+  end
+
+  def business_time_passed_from_beginning
+    time = Time.parse(self.to_s)
+    workday = Time.workday?(time)
+    if workday && Time.during_business_hours?(time)
+      self - Time.beginning_of_workday(time)
+    elsif Time.after_business_hours?(time) && workday
+      Time.end_of_workday(time) - Time.beginning_of_workday(time)
+    else
+      0
+    end
+  end
+
 end
--- test/test_time_extensions.rb
+++ test/test_time_extensions.rb
@@ -38,5 +38,94 @@ class TestTimeExtensions < Test::Unit::TestCase
     expecting = Time.parse("August 17th, 2010, 5:00 pm")
     assert_equal expecting, Time.end_of_workday(first)
   end
+
+  should "know if instance is during business hours" do
+    assert(!Time.during_business_hours?(Time.parse("April 11, 2010 10:45 am")))
+    assert(!Time.during_business_hours?(Time.parse("April 12, 2010  8:45 am")))
+    assert(Time.during_business_hours?(Time.parse("April 12, 2010  9:45 am")))
+  end
+
+
+  should "know a seconds to an instance" do
+    start_time = Time.parse("August 17th, 2010, 8:50 am")
+    end_time = Time.parse("August 17th, 2010, 13:00 pm")
+    expecting = start_time.business_time_left_to(end_time)
+    assert_equal expecting, 3600 * 4
+  end
+
+  should "know a seconds to an instance" do
+    start_time = Time.parse("August 17th, 2010, 11:50 am")
+    end_time = Time.parse("August 17th, 2010, 13:50 pm")
+    expecting = start_time.business_time_left_to(end_time)
+    assert_equal expecting, 7200
+  end
+
+  should "know a seconds to an instance" do
+    start_time = Time.parse("August 17th, 2010, 17:50 am")
+    end_time = Time.parse("August 17th, 2010, 18:50 pm")
+    expecting = start_time.business_time_left_to(end_time)
+    assert_equal expecting, 0
+  end
+
+  should "know a seconds to an instance in weekend" do
+    start_time = Time.parse("August 22th, 2010, 11:50 am")
+    end_time = Time.parse("August 22th, 2010, 13:50 pm")
+    expecting = start_time.business_time_left_to(end_time)
+    assert_equal expecting, 0
+  end
+
+  should "know a seconds to an instance between holidays" do
+    start_time = Time.parse("August 22th, 2010, 11:50 am")
+    BusinessTime::Config.reset
+    
+    BusinessTime::Config.holidays << Date.parse("August 24, 2010")
+    end_time = Time.parse("August 24th, 2010, 13:50 pm")
+    expecting = start_time.business_time_left_to(end_time)
+    assert_equal expecting, 8 * 3600
+  end
+
+  should "know a seconds to an other day instance" do
+    start_time = Time.parse("August 17th, 2010, 16:00 pm")
+    end_time = Time.parse("August 18th, 2010, 9:00 am")
+    expecting = start_time.business_time_left_to(end_time)
+    assert_equal expecting, 3600
+  end
+
+  should "know a seconds to an other week instance" do
+    start_time = Time.parse("August 17th, 2010, 16:00 pm")
+    end_time = Time.parse("August 23th, 2010, 10:00 am")
+    expecting = start_time.business_time_left_to(end_time)
+    assert_equal expecting, (3600 * 26)
+  end
+
+  should "know a seconds to end of business hours" do
+    time = Time.parse("August 17th, 2010, 16:00 pm")
+    expecting = time.business_time_left_to_end
+    assert_equal expecting, 3600
+    time = Time.parse("August 17th, 2010, 8:00 am")
+    expecting = time.business_time_left_to_end
+    assert_equal expecting, 3600 * 8
+    time = Time.parse("August 17th, 2010, 18:00 pm")
+    expecting = time.business_time_left_to_end
+    assert_equal expecting, 0
+    time = Time.parse("August 21th, 2010, 16:00 pm")
+    expecting = time.business_time_left_to_end
+    assert_equal expecting, 0
+  end
+
+  should "know a seconds from beginning of business hours" do
+    time = Time.parse("August 17th, 2010, 16:00 pm")
+    expecting = time.business_time_passed_from_beginning
+    assert_equal expecting, 3600 * 7
+    time = Time.parse("August 17th, 2010, 8:00 am")
+    expecting = time.business_time_passed_from_beginning
+    assert_equal expecting, 0
+    time = Time.parse("August 17th, 2010, 18:00 pm")
+    expecting = time.business_time_passed_from_beginning
+    assert_equal expecting, 3600 * 8
+    time = Time.parse("August 21th, 2010, 16:00 pm")
+    expecting = time.business_time_passed_from_beginning
+    assert_equal expecting, 0
+  end
   
 end
